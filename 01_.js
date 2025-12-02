08b2cc0c4fde9240f6754969aed9985f5e921360
Ashika J <106242789+Ashikajayakumar@users.noreply.github.com>
2025-12-02 13:06:17 +0530

    Update and rename 01_.js to 01_.md

--- 01_.js
+++
@@ -1,7 +0,0 @@
-                                                             How to display the HELLO WORLD 
-
- 1.Console.log(" "):
-
-*  Console.log  - Built-in JavaScript function used to print message to the browser's console .
-*  console - refer to browser's debugging Console.
-*  log - is the method of the console object that output a message.
